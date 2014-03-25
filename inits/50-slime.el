b98e52fb640428562d8aa10e09ed2eefa8a123ba
lassie <lassie116@gmail.com>
2014-03-26 06:39:57 +0900

    bugfix

--- inits/50-slime.el
+++ inits/50-slime.el
@@ -3,7 +3,7 @@
   ;;(setq inferior-lisp-program "/usr/bin/clisp")
   (setq inferior-lisp-program "/usr/bin/sbcl")
   (add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
-  (require 'slime)
+  ;(require 'slime)
   (slime-setup))
 
 (slime-setting)
